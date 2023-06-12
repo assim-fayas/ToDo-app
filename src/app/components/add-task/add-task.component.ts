1a76da608eae37b257adb9173ae511fa1f358dc1
assim fayas <asim1fayas@gmail.com>
2023-06-12 07:35:36 +0530

    fixed all bugs

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,6 @@
 import { Component,Output,EventEmitter } from '@angular/core';
 import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs'
 import {Task} from '../../Task'
 @Component({
   selector: 'app-add-task',
@@ -11,8 +12,14 @@ export class AddTaskComponent {
 text!:string;
 day!:string;
 reminder:boolean=false;
+showAddTask!:boolean;
+subscription!:Subscription;
 
-constructor(private uiService:UiService){}
+constructor(private uiService:UiService){
+  this.subscription=this.uiService
+  .onToggle()
+  .subscribe((value)=>(this.showAddTask=value));
+}
 
 onSubmit(){
   if(!this.text){
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,22 +1,29 @@
 import { Component } from '@angular/core';
-import{UiService} from '../../services/ui.service'
+import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  title:string = 'Task Tracker';
-  showAddTask!:boolean;
-  subscription!:Subscription;
+  title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription!: Subscription;
 
-  constructor(private uiService:UiService){
-    this.subscription=this.uiService.onToggle().subscribe((value)=>(this.showAddTask=value));
+  constructor(private uiService: UiService, private router: Router) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
   }
 
-    toggleAddTask(){
-     this.uiService.toggleAddTask();
-      
-    }
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
 }
